52d2dd14fcb651240972c78439be1e06b627d0b5
codeTillMorn <jatinbigb6@gmail.com>
2021-04-16 22:54:44 +0530

    modified 2nd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDom from "react-dom"
+import React from "react"
+import ReactDOM from "react-dom"
 import App from "./components/App"
 import "bootstrap/dist/css/bootstrap.min.css"
-ReactDom.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
