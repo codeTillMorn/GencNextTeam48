d7668e59fbd0c0f64994b06044d471739e79a4fa
codeTillMorn <jatinbigb6@gmail.com>
2021-05-07 16:56:55 +0530

    saving pdf (jatin)

--- src/components/Dictaphone1.js
+++ src/components/Dictaphone1.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect} from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import { Button} from "react-bootstrap"
+import { jsPDF } from "jspdf";
 
 const Dictaphone1 = () => {
 //  const [message, setMessage] = useState('');
@@ -45,8 +46,15 @@ const Dictaphone1 = () => {
    });
   }
    const stopListen = () => {
+    console.log("stopping and saving pdf");
     SpeechRecognition.stopListening();
 
+ };
+ const savePdf = () => {
+
+  var doc = new jsPDF();
+  doc.text(10, 10, finalTranscript);
+  doc.save('transcript.pdf');
  };
 
  return (
@@ -58,10 +66,12 @@ const Dictaphone1 = () => {
          {listening ? 'on' : 'off'}
        </span>
        <div>
-         <Button type="button" variant="outline-secondary" onClick={resetTranscript}>Reset</Button>{"    "}
-         <Button color="#70ff5c" variant="outline-success" onClick={listenContinuously}  >Start</Button>{"      "}
-         <Button type="button" variant="outline-danger" onClick={stopListen}>Stop</Button>{"     "}
+         <Button type="button" variant="outline-secondary" onClick={resetTranscript}>Reset</Button>{"  "}
+         <Button color="#70ff5c" variant="outline-success" onClick={listenContinuously}  >Start</Button>{"  "}
+         <Button type="button" variant="outline-danger" onClick={stopListen}>Stop</Button>{"  "}
        </div>
+       <br/>
+       <Button type="button" variant="outline-primary" onClick={savePdf}>SAVE pdf</Button>{"  "}
      </div>
      {/* <div>
        {message}
--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,7 +1,9 @@
-import React, { useRef, useState } from "react"
-import { Form, Button, Card, Alert, InputGroup, FormControl} from "react-bootstrap"
+import React from "react"
+import { Form, Card} from "react-bootstrap"
 import Dictaphone1 from './Dictaphone1.js'
 // import default transcript from './Dictaphone1.js';
+
+
 
 function FileUpload() {
   
@@ -15,27 +17,9 @@ return (
             <Form.File id="exampleFormControlFile1" label="Upload audio file" />
           </Form.Group>
         </Form>
-        {/* <Button color="#70ff5c" variant="outline-success">Start</Button> */}
         <Dictaphone1 />
         </Card.Body>
         </Card>
-<div>
-      <InputGroup>
-    <InputGroup.Prepend>
-      <InputGroup.Text />
-    </InputGroup.Prepend>
-    <FormControl   />
-  </InputGroup>
-
-</div>
-
-<h2>{}</h2>
-
-{/* <div className="App">
-      <header className="Dictaphone-Tester">
-        <Dictaphone1 />
-      </header>
-    </div> */}
       </>
     );
 }
